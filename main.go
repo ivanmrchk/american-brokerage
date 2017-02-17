567172e80c774088bbb8619dcc1c2d8e8f9663dd
Ivan Marchenko <imarchenko@gmail.com>
2017-02-17 23:18:19 +0200

    add templates and / route

--- main.go
+++ main.go
@@ -2,30 +2,55 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
+
+	_ "github.com/julienschmidt/httprouter"
 )
 
-func init() {
-	http.HandleFunc("/", dog)
-	http.Handle("/favicon.ico", http.NotFoundHandler())
-	//http.ListenAndServe(":8080", nil)
+var tpl *template.Template
+
+func main() {
+
+	// parse template dir
+	tpl = template.Must(template.ParseGlob("templates/*"))
+
+	// set new router
+	//mux := httprouter.New()
+
+	// define routes
+	http.HandleFunc("/", index)
+
+	//mux.ServeFiles("/public", http.Dir(""))
+	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("./public"))))
+
+	http.ListenAndServe(":8080", nil)
 
 }
 
-func dog(w http.ResponseWriter, r *http.Request) {
+// root route handler
+func index(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/" {
 		errorHandler(w, r, http.StatusNotFound)
 		return
 	}
+	// execute tamplate
+	err := tpl.ExecuteTemplate(w, "index.gohtml", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
 
-	fmt.Fprintln(w, "Nothing here, but very soon.")
 }
 
 func errorHandler(w http.ResponseWriter, r *http.Request, status int) {
 	w.WriteHeader(status)
 	if status == http.StatusNotFound {
-		fmt.Fprint(w, "404 Page not found")
+		err := tpl.ExecuteTemplate(w, "404.gohtml", nil)
+		if err != nil {
+			fmt.Println(err)
+		}
 	}
+
 }
 
 //goapp deploy -application american-brokerage-158918 -version 1 .
