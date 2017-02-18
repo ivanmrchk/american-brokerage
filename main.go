09d164cc014dcf8db7ebbf50b36d6648fd0c1b48
Ivan Marchenko <imarchenko@gmail.com>
2017-02-18 16:59:49 +0200

    add mail functionalities and mail template

--- main.go
+++ main.go
@@ -1,14 +1,26 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"html/template"
+	"log"
 	"net/http"
+
+	gomail "gopkg.in/gomail.v2"
 
 	"github.com/julienschmidt/httprouter"
 )
 
 var tpl *template.Template
+
+type quoteForm struct {
+	Name    string
+	Email   string
+	Phone   string
+	Subject string
+	Message string
+}
 
 func main() {
 
@@ -43,13 +55,30 @@ func index(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 }
 func postIndex(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	fmt.Println("a")
+
+	r.ParseForm()
+
 	// execute tamplate
 	err := tpl.ExecuteTemplate(w, "index.gohtml", nil)
 
 	if err != nil {
 		fmt.Println(err)
 	}
+
+	// quote form data
+	q := quoteForm{Name: r.FormValue("qname"),
+		Email:   r.FormValue("qemail"),
+		Phone:   r.FormValue("qphone"),
+		Subject: r.FormValue("qsubject"),
+		Message: r.FormValue("qmessage"),
+	}
+
+	// grab Name from q
+	fromN := q.Name
+	fromE := q.Email
+
+	// send email
+	q.sendMail(fromN, fromE)
 
 }
 
@@ -75,7 +104,7 @@ func privacy(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
 
 }
 
-// custom 404 route hanler
+// custom 404 route handler
 func customNotFound(w http.ResponseWriter, r *http.Request) {
 	fmt.Println(r.URL.Path, "not found route")
 
@@ -86,6 +115,45 @@ func customNotFound(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Println(r.Method)
 
+}
+
+// send quote to email
+func (q quoteForm) sendMail(fromName string, fromEmail string) {
+
+	// execute email template
+	t := template.New("send-q.gohtml")
+
+	var err error
+	t, err = t.ParseFiles("templates/send-q.gohtml")
+	if err != nil {
+		log.Println(err)
+	}
+
+	// convert email to string
+	var tpl bytes.Buffer
+	if err := t.Execute(&tpl, q); err != nil {
+		log.Println(err)
+	}
+	result := tpl.String()
+
+	// prepare email
+	m := gomail.NewMessage()
+	m.SetHeader("From", fromEmail)
+	m.SetHeader("To", "americanbrokerageapp@gmail.com")
+	//m.SetAddressHeader("Cc", "dan@example.com", "Dan")
+	m.SetHeader("Subject", fromName+" Has requested a quote fon your website.")
+	m.SetBody("text/html", result)
+	//m.Attach("template.html")
+
+	// prepare dialer
+	d := gomail.NewDialer("smtp.gmail.com", 587, "americanbrokerageapp@gmail.com", "M)FrFC=pwR8(<?#B")
+
+	// send email
+	if err := d.DialAndSend(m); err != nil {
+		panic(err)
+	}
+
+	log.Println("mail sent", q)
 }
 
 //goapp deploy -application american-brokerage-158918 -version 1 .
